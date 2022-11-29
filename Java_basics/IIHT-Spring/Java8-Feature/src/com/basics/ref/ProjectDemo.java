0820ed362c3efb44109507f9ef6f7d7e367dc2ee
unknown <cogjava4578@iiht.tech>
2022-11-29 17:00:55 +0530

    added method ref

--- Java_basics/IIHT-Spring/Java8-Feature/src/com/basics/ref/ProjectDemo.java
+++ Java_basics/IIHT-Spring/Java8-Feature/src/com/basics/ref/ProjectDemo.java
@@ -10,4 +10,28 @@ public class ProjectDemo {
 		System.out.println("on " + LocalDate.now());
 	}
 
+	public static void threadTask() {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i * 2);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void oddTo100() {
+		for (int i = 1; i <= 100; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
--- Java_basics/IIHT-Spring/Java8-Feature/src/com/basics/ref/RefDemo.java
+++ Java_basics/IIHT-Spring/Java8-Feature/src/com/basics/ref/RefDemo.java
@@ -5,8 +5,16 @@ public class RefDemo {
 	public static void main(String[] args) {
 		System.out.println("Method reference demo");
 		//provide the implementation of IWork
-		IWork work = ProjectDemo::projectInprogress;
-		work.doProject();
+//		IWork work = ProjectDemo::projectInprogress;
+//		work.doProject();
+//		
+//		Runnable run = ProjectDemo::threadTask;
+//		Thread t1 = new Thread(run);
+//		t1.start();
+		
+		Runnable run = ProjectDemo::oddTo100;
+		Thread t1 = new Thread(run);
+		t1.start();
 	}
 
 }
