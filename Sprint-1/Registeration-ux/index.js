c753e4f19b6c692af6b650bd2fae5ec582475d4f
unknown <cogjava4578@iiht.tech>
2022-12-05 11:20:37 +0530

    sprint 1 - console printing and validation page

--- Sprint-1/Registeration-ux/index.js
+++ Sprint-1/Registeration-ux/index.js
@@ -1,3 +1,21 @@
-function greetings(){
-    console.log("hi");
+function getFormValue(ev) {
+    // Disable default form submission action
+    ev.preventDefault();
+    // Pick the form reference
+    const form = document.getElementById('form1');
+    // get all elements like input and select, you can add many more
+    const allInputs = form.querySelectorAll('input,select');
+    let formObj = {};
+    // Get the values of each field
+    allInputs.forEach(e => {
+        //All key depends on the name in the element
+        const key = e.getAttribute('name');
+        formObj[key] = e.value;
+    });
+    // var a = document.getElementById('fname').value;
+    // console.log(a);
+    // console.log(formObj);
+    localStorage.setItem("form1",JSON.stringify(formObj));
+    // window.location.replace('/thankyou.html');
+    setTimeout("location.href = '/thankyou.html';",1000);
 }
---
+++ Sprint-1/Registeration-ux/thankyou.js
@@ -0,0 +1,4 @@
+//load the data or do the activity when page loaded
+window.onload = () => {
+    console.log(JSON.parse(localStorage.getItem("form1")));
+};
