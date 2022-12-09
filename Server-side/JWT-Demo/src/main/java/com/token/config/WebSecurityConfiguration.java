f4bb9e5cd309371fedaf67cb645629fc2878240c
unknown <cogjava4578@iiht.tech>
2022-12-09 17:53:27 +0530

    jwt-config-update

--- Server-side/JWT-Demo/src/main/java/com/token/config/WebSecurityConfiguration.java
+++ Server-side/JWT-Demo/src/main/java/com/token/config/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -29,4 +30,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		return super.authenticationManager();
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+		.disable()
+		.authorizeRequests()
+		.antMatchers("/auth")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
+	}
 }
