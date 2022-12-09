164157cab153ad47ec37455aa60ff266942b5bb2
unknown <cogjava4578@iiht.tech>
2022-12-09 15:19:39 +0530

    spring security web fix

--- Server-side/JWT-Demo/src/main/java/com/token/services/UserService.java
+++ Server-side/JWT-Demo/src/main/java/com/token/services/UserService.java
@@ -1,16 +1,19 @@
 package com.token.services;
 
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService implements UserDetailsService{
+public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		//logic to get the username and password from database can be written here
+		// logic to get the username and password from database can be written here
 		return new User("admin", "password", new ArrayList<>());
 	}
 
