52253225e8543539ed20c34c20f741deb095723e
unknown <cogjava4578@iiht.tech>
2022-12-15 17:36:30 +0530

    added post-api

--- Client-side/helloworld/src/app/Components/userform/userform.component.ts
+++ Client-side/helloworld/src/app/Components/userform/userform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import User from 'src/app/Entity/user';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-userform',
@@ -8,17 +9,25 @@ import User from 'src/app/Entity/user';
 })
 export class UserformComponent implements OnInit {
 
-  title: String = "Please fill the form below"
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+
+  title: String = "Please fill the form below";
 
   user: User = new User();
 
   save() {
-    console.log("name " + this.user.firstname + " " + this.user.lastname);
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
+    const observables = this.userService.saveUser(this.user);
+    observables.subscribe(
+      (res: any) => {
+        console.log(res);
+      }, function (error) {
+        console.log(error);
+        alert("Something went wrong !, Please try again");
+      }
+    )
   }
 
 }
--- Client-side/helloworld/src/app/Services/user.service.ts
+++ Client-side/helloworld/src/app/Services/user.service.ts
@@ -1,10 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const BASE_URL="http://localhost:5000";
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor(private http: HttpClient) { }
+
+  saveUser(user: {
+    firstname: String;
+    lastname: String;
+    age: number;
+    gender: String;
+    email: String;
+  }) {
+       return this.http.post(BASE_URL+"/users", user);
+  }
 }
